7a46e2bda02857988eb3bf5f1b1b89def230cb28
Lars Nørregaard <lnorregaard@nordija.com>
2017-03-28 23:01:42 +0200

    Moved << to doLast as it is going to be deprecated

--- src/main/groovy/com/nordija/VersionManagerPlugin.groovy
+++ src/main/groovy/com/nordija/VersionManagerPlugin.groovy
@@ -17,26 +17,30 @@ class VersionManagerPlugin implements Plugin<Project> {
             group = 'task'
             description = 'Find the version from git system'
         }
-        project.tasks.showVersion << {
-            println "Version (project.version): " + project.version
-            println "Branch (System.properties.gitBranch): " + System.properties.gitBranch;
-            println "Global commit count (System.properties.gitCommitCount): " + System.properties.gitCommitCount;
-            println "Highest tag hash (System.properties.gitHighestTagHash): " + System.properties.gitHighestTagHash;
-            println "Highest tag (System.properties.gitHighestTag): " + System.properties.gitHighestTag;
-            println "Highest tag count (System.properties.gitHighestTagCount): " + System.properties.gitHighestTagCount;
-            println "Current commit short hash (System.properties.gitCurrentShortCommitHash): " + System.properties.gitCurrentShortCommitHash;
-            println "Current commit hash (System.properties.gitCurrentCommitHash): " + System.properties.gitCurrentCommitHash;
-            println "Derived values based on above information: "
-            println "Use in maven version and gradle version (System.properties.mavenVersion): " + System.properties.mavenVersion;
-            println "Use in app version (System.properties.appVersion): " + System.properties.appVersion;
-            println "Use as part of artifact name (System.properties.gitDescribe): " + System.properties.gitDescribe;
-            println "Use as part of artifact name (System.properties.gitAppDescribe): " + System.properties.gitAppDescribe;
-            println "Use as versionGitNumber (System.properties.gitPaddedVersionCount): " + System.properties.gitPaddedVersionCount;
-            println "Use as part of artifact name (System.properties.versionSnapshot): " + System.properties.versionSnapshot;
+        project.tasks.showVersion  {
+            doLast {
+                println "Version (project.version): " + project.version
+                println "Branch (System.properties.gitBranch): " + System.properties.gitBranch;
+                println "Global commit count (System.properties.gitCommitCount): " + System.properties.gitCommitCount;
+                println "Highest tag hash (System.properties.gitHighestTagHash): " + System.properties.gitHighestTagHash;
+                println "Highest tag (System.properties.gitHighestTag): " + System.properties.gitHighestTag;
+                println "Highest tag count (System.properties.gitHighestTagCount): " + System.properties.gitHighestTagCount;
+                println "Current commit short hash (System.properties.gitCurrentShortCommitHash): " + System.properties.gitCurrentShortCommitHash;
+                println "Current commit hash (System.properties.gitCurrentCommitHash): " + System.properties.gitCurrentCommitHash;
+                println "Derived values based on above information: "
+                println "Use in maven version and gradle version (System.properties.mavenVersion): " + System.properties.mavenVersion;
+                println "Use in app version (System.properties.appVersion): " + System.properties.appVersion;
+                println "Use as part of artifact name (System.properties.gitDescribe): " + System.properties.gitDescribe;
+                println "Use as part of artifact name (System.properties.gitAppDescribe): " + System.properties.gitAppDescribe;
+                println "Use as versionGitNumber (System.properties.gitPaddedVersionCount): " + System.properties.gitPaddedVersionCount;
+                println "Use as part of artifact name (System.properties.versionSnapshot): " + System.properties.versionSnapshot;
+            }
         }
-        project.tasks.findVersion << {
-            project.tasks.version.execute();
-            println "Version: " + project.version
+        project.tasks.findVersion  {
+            doLast {
+                project.tasks.version.execute();
+                println "Version: " + project.version
+            }
         }
 
     }
